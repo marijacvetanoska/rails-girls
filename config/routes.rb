1637a403b353811e3f985e584f679b1691934f5a
Marija Cvetanoska <cvetanoska.marija01@gmail.com>
2019-04-13 15:40:31 +0200

    ftnkytmtky

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :comments
   get 'pages/info'
-  root to: 'ideas#index'
+  root to: 'pages#info'
   resources :ideas
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
